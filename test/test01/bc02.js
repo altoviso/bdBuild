8962baeb905a582b66883240e09fffd7d9817559
Rawld Gill <rgill@altoviso.com>
2011-02-17 11:30:38 -0800

    interim; new control engine is working (user: rcgill tags: trunk)

--- test/test01/bc02.js
+++ test/test01/bc02.js
@@ -1,10 +1,10 @@
 // this script tests files, dirs, and trees with baseDir set to an absolute path
 {
   // when running this test, make sure the following path is set appropriately for your installation
-  baseDir:"/home/rcgill/dev/bdBild/test/test01",
+  baseDir:"/home/rcgill/dev/bdBuild/test/test01",
   
   files:[
-    ["../../../bdLoad/lib/require.js", "./require.js"]
+    "./dir-to-optionally-copy/resource-to-optionally-copy-1.js"
   ],
 
   dirs:[
@@ -12,6 +12,7 @@
   ],
   
   trees:[
-    ["./css", "./css"]
+    ["./css", "./css"],
+    ["/home/rcgill/dev/dojotoolkit", "./dojo", "*/.*"]
   ]
 }

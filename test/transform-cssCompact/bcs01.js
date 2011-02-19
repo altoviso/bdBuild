e0067d59c4c1627c7b8263f5a8a9db7a2abc6750
Rawld Gill <rgill@altoviso.com>
2011-02-19 08:23:15 -0800

    added compact css transform (user: rcgill tags: trunk)

---
+++ test/transform-cssCompact/bcs01.js
@@ -0,0 +1,17 @@
+// this script tests files, dirs, and trees with pair items
+{
+  files:[
+    ["../../../backdraft/demo/appFrame/css.css", "./css.css"]
+  ],
+
+  trees:[
+    ["../../../dojotoolkit/dojo/resources", "./cssResources"],
+    ["../../../dojotoolkit/dijit/themes", "./cssResources"],
+    ["../../../dojotoolkit/dijit/icons", "./cssResources"],
+    ["../../../backdraft/lib/themes", "./cssResources"]
+  ],
+
+  compactCssSet:{
+    "../../../backdraft/demo/appFrame/css.css":1
+  }
+}
